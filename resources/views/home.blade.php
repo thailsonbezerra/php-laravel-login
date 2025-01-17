ec07483e010a669f29967a6c94b7c6905ea84410
Thailson Bezerra <thailsonbezerra@outlook.com>
2025-01-17 00:11:40 -0300

    style: content da page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,8 +22,19 @@
                 </div>
             </div>
         </nav>
-        {{-- <h1>
-            Que tal organizar seus <span>escopos</span> de forma simples e eficiente?
-        </h1> --}}
+
+        <div class="container w-80 vh-100">
+            <div class="content h-100 d-flex flex-column justify-content-center align-items-center text-center px-4">
+                <h1 class="h2">
+                    Que tal organizar seus <span class="highlight-title">projetos</span> de forma simples e eficiente?
+                </h1>
+
+                <p>
+                    Escopar é uma ferramenta que te ajuda a organizar seus projetos de forma simples e eficiente.
+                </p>
+                <a href="#" class="btn btn-salmon-light">Começar</a>
+            </div>
+        </div>
+
     </body>
 </html>
